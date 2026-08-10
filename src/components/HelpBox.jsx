1f6314b58115f72f8dbed6d399181eebbd128940
jnagendra10 <jnagendra.sdce@gmail.com>
2026-08-10 22:32:02 +0530

    activity type test15

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -5,7 +5,7 @@ import './HelpBox.css';
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
-      <h6>{title}</h6>
+      <h3>{title}</h3>
       <p>{text}</p>
     </article>
   );
