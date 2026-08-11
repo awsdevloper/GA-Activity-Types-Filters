5630009d3c09a91f069835d3fd2ca74eea1ca215
awsdevloper <jnagendra.cloud@gmail.com>
2026-08-11 13:52:36 +0530

    fork test1

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -5,7 +5,7 @@ import './HelpBox.css';
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
-      <h10>{title}</h10>
+      <h11>{title}</h11SSSS>
       <p>{text}</p>
     </article>
   );
